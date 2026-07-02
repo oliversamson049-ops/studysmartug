f531efe21aae87a295aefe6edd8aafd075b1e87b
oliversamson049-ops <oliversamson049@gmail.com>
2026-07-02 06:48:47 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://wizwmfatayuozhcbnuda.supabase.co";
-const supabaseKey = "sb_publishable_hQuehWhyYTcJIlU4Y0T31w_uG5JQI-P";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
