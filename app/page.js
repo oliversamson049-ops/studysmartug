d07611e788bb953cdb627fa7e22cd21b6ce85819
oliversamson049-ops <oliversamson049@gmail.com>
2026-07-02 06:57:38 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">StudySmart UG</h1>
+      <p>Site is live ✅</p>
+    </main>
+  )
+}
