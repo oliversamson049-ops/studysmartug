404a4eadf328f3349933aa3c2eed2c700ccd8496
oliversamson049-ops <oliversamson049@gmail.com>
2026-07-02 07:05:50 +0300

    Create page.js

---
+++ app/courses/page.js
@@ -0,0 +1,13 @@
+export default function Courses() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Courses</h1>
+      <ul className="list-disc pl-6 space-y-2">
+        <li>Mathematics P1 & P2 - S4 to S6</li>
+        <li>Physics - S5 & S6</li>
+        <li>Economics - S4 to S6</li>
+        <li>More subjects coming soon...</li>
+      </ul>
+    </main>
+  )
+}
